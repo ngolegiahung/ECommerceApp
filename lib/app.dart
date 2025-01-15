a266618036a30c08378220ebaf0d9e6ff8925c93
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2025-01-15 21:49:27 +0700

    Create some helper function, constant, http, validation and ...

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      home: const Text("data"),
+    );
+  }
+}
