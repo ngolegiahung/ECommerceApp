25210b6f42d3c2e59db296bbda250b71c0461b43
Hung L.G. NGO <hung.l.g.ngo@gmail.com>
2025-01-19 10:45:48 +0700

    Custom Clip Path at Home screen.

--- lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -1,10 +1,10 @@
-import 'package:e_commerce_app/features/authentication/controllers/onboarding/onboarding_controller.dart';
-import 'package:e_commerce_app/utils/constants/colors.dart';
-import 'package:e_commerce_app/utils/constants/sizes.dart';
-import 'package:e_commerce_app/utils/device/device_utility.dart';
-import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/device/device_utility.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:e_commerce_app/features/authentication/controllers/onboarding/onboarding_controller.dart';
 
 class OnBoardingDotNavigation extends StatelessWidget {
   const OnBoardingDotNavigation({
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:e_commerce_app/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [Container()],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
